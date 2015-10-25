d1136f1982a8e7d1cd49335b2cfb4c597a2e8106
Pawel <pawelpedzinski1a@gmail.com>
2015-10-25 20:10:13 +0100

    Dodałem klasę Physics, w której znajduje się metoda odpowiedzialna za obliczanie sił grawitacyjnych oddziałowujących na obiekty

---
+++ BodyModel.py
@@ -0,0 +1,14 @@
+from PhysicsVariables import *
+__author__ = 'Pawel'
+
+
+class BodyModel:
+    #statyczny licznik modeli
+    id = 0
+
+    def __init__(self, acceleration = Acceleration(0,0), velocity = Velocity(0,0), position = Position(0,0), mass = 0):
+        BodyModel.id += 1
+        self.acceleration = acceleration
+        self.velocity = velocity
+        self.position = position
+        self.mass = mass
---
+++ PhysicsVariables.py
@@ -0,0 +1,22 @@
+__author__ = 'Pawel'
+
+
+class Force:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Acceleration:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Velocity:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
