0f6e64f1fc5bfcb400c333888e1c6d631256ee74
Pawel <pawelpedzinski1a@gmail.com>
2015-11-18 19:18:24 +0100

    Dodana implementacja serwera dla udp i tcp

--- core/server/server.py
+++ core/server/server.py
@@ -1,7 +1,13 @@
 __author__ = 'Pawel'
 
 from twisted.internet import reactor, protocol
+from twisted.internet.protocol import DatagramProtocol
 
+
+class EchoUDP(DatagramProtocol):
+    def datagramReceived(self, datagram, address):
+        print(datagram)
+        self.transport.write(datagram, address)
 
 class Echo(protocol.Protocol):
     """This is just about the simplest possible protocol"""
@@ -9,13 +15,18 @@ class Echo(protocol.Protocol):
     def dataReceived(self, data):
         "As soon as any data is received, write it back."
         self.transport.write(data)
+        print("dataReceived")
 
 
 def main():
     """This runs the protocol on port 8000"""
     factory = protocol.ServerFactory()
     factory.protocol = Echo
-    reactor.listenTCP(8000,factory)
+    reactor.listenTCP(8009,factory)
+
+    #reactor.run()
+
+    reactor.listenUDP(8008, EchoUDP())
     reactor.run()
 
 # this only runs if the module was *not* imported
